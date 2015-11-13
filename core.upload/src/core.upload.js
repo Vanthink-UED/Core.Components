84b7d1febd94ad8ee35e3a99ae86b6f9427d6ba6
JackPu <kakashjack@gmail.com>
2015-11-13 22:33:26 +0800

    add ajax support

--- core.upload/src/core.upload.js
+++ core.upload/src/core.upload.js
@@ -13,6 +13,8 @@
             url: "/",
             title: '选择文件',
             data: !1,
+            maxFileSize: null,
+            type: 'normal',
             onSubmit: function() {
                 return !0
             },
@@ -31,7 +33,6 @@
             };
             var c = a('<form  method="' + b.method + '" enctype="multipart/form-data" action="' + b.url + '"> <input name="' + b.name + '" type="file" accept="' + b.acceptEx + '" /></form>'),
                 h = c.find("input[name=" + b.name + "]");
-            console.log(b.name);
             h.css({
                 display: "block",
                 position: "absolute",
@@ -46,17 +47,30 @@
                 "z-index": 10,
                 overflow: "hidden"
             }).attr("title", b.title);
-            h.on("change", function(d) {
+            h.on("change", function(fe) {
                 d = h.val().replace(/C:\\fakepath\\/i, "");
                 d = d.substring(d.lastIndexOf(".") + 1);
                 if (!b.accept.test(d)) {
                     return b.extError.call(e, this), c.get(0).reset(), !1
+                }
+                if (fe.target.files[0].size > b.maxFileSize) {
+                    var formatSize;
+                    if (parseInt(b.maxFileSize / 1024 / 1024) > 0) {
+                        formatSize = (b.maxFileSize / 1024 / 1024).toFixed(2) + 'MB';
+                    } else if (parseInt(b.maxFileSize / 1024) > 0) {
+                        formatSize = (b.maxFileSize / 1024).toFixed(2) + 'kB';
+                    } else {
+                        formatSize = b.maxFileSize.toFixed(2) + 'Byte';
+                    }
+                    alert('文件不能超过(' + formatSize + ')');
+                    return;
                 }
                 c.find("input[type=hidden]").remove();
                 b.onSubmit.call(e, a(this));
                 b.data && a.each(b.data, function(b, d) {
                     c.append(a('<input type="hidden" name="' + b + '" value="' + d + '">'))
                 });
+
                 c.submit();
                 a(c).find("input[type=file]").attr("disabled", "disabled")
             });
@@ -93,25 +107,24 @@
                 maximumSize: 1024,
                 enableMaximumSize: false,
                 enableButton: false,
-                enableDrag: false,
                 inputOfFile: 'file',
-                
+                type: '',
                 uploadedCallback: function(response) {}
             };
-    	
+
             var Options = $.extend(defaults, Options);
             if(Options.extensions.length == 0){
-                return UploadLoading.show('文件扩展不能为空');
+                return alert('文件扩展不能为空 extensions');
             }
             if(!Options.actionToSubmitUpload){
-                return UploadLoading.show('"actionToSubmitUpload"必须指定上传地址');
+                return console.error('"actionToSubmitUpload"必须指定上传地址');
             }
-           
+
             return this.each(function() {
                 var Elemento;
-                
-                
-                   
+
+
+
                 var initUpload = function(element) {
                     element.css({
                         "position": "relative",
@@ -136,14 +149,14 @@
                         element.on("mouseout", function(e) {
                           //  element.css('border','4px dashed #ccc');
                             e.stopPropagation();
-                        });    
+                        });
                     }
-                   
+
 
                     var $inputHidden = $("<input type='hidden' name='" + Options.inputOfFile + "' id='" + Options.inputOfFile  + "'>");
                     $inputHidden.addClass("picture-element-image-directory");
                     element.append($inputHidden);
-                  
+
                 };
                 var getExt = function(name) {
                     return name.slice(name.lastIndexOf(".") + 1);
@@ -159,15 +172,17 @@
                         method: 'POST',
                         url: Options.actionToSubmitUpload,
                         data: post,
+                        type: Options.type,
+                        maxFileSize: Options.maximumSize,
                         onSubmit: function() {
-                            // use svg icon 
+                            // use svg icon
                             var icon_str = '<div class="core-upload-svg-icon" style="position:absolute;left:0;top:0;width:100%;height:100%;text-align:center;background-color:';
                                 icon_str += element.css('background-color');
                                 icon_str += ';">';
                                 icon_str += '<svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="25px" height="25px" style="vertical-align:middle;" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve"><path fill="#fff" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z" transform="rotate(293.601 25 25)"><animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"></animateTransform></path></svg>';
                                 icon_str += '上传中...</div>';
                             $loading = $(icon_str);
-                            
+
                             element.append($loading);
                         },
                         onComplete: function(response) {
@@ -175,11 +190,11 @@
                             $loading.remove();
                             $loading = '';
                             if(response.errno == '0'){
-                                Options.uploadedCallback(response);    
+                                Options.uploadedCallback(response);
                             }else{
-                                alert(response.errmsg); 
+                                alert(response.errmsg);
                             }
-                            
+
 
                         },
                         extError: function() {
@@ -192,8 +207,8 @@
                         element.removeClass("TuyoshiImageUpload_div")
                     })
                 };
-                
-                
+
+
                 $elemenTo = $(this);
                 initUpload($elemenTo);
                 startAjaxUpload($elemenTo);
@@ -213,9 +228,9 @@
     };
     $.fn.CoreUpload = function(MetodoOuOptions) {
         if (methods[MetodoOuOptions]){
-            return methods[MetodoOuOptions].apply(this, Array.prototype.slice.call(arguments, 1)); 
+            return methods[MetodoOuOptions].apply(this, Array.prototype.slice.call(arguments, 1));
         }else if (typeof MetodoOuOptions === 'object' || !MetodoOuOptions) {
-            return methods.init.apply(this, arguments);  
+            return methods.init.apply(this, arguments);
         }
     };
 })(jQuery);
